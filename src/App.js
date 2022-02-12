7ea00ddbe274bf21ee19b788c90aec05e1d86f64
afranceschetti <alessandro.franceschetti@csi.it>
2022-02-12 13:20:23 +0100

    i18n

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component, Suspense } from 'react';
 import 'bulma/css/bulma.min.css';
-import {
-  Route,
-  NavLink,
-  HashRouter,
-  Routes
-} from "react-router-dom";
+import { withTranslation } from 'react-i18next';
+import { Route, NavLink, HashRouter, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Contact from "./pages/Contact";
 
-function App() {
-  return (
-    <HashRouter>
-      <div>
-        <h1>Simple SPA</h1>
-        <ul className="header">
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/profile">Profile</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
-        </ul>
-        <div className="content">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
+class MainApp extends Component {
+
+  changeLanguage(lang) {
+    const { i18n } = this.props;
+    i18n.changeLanguage(lang);
+  }
+
+  render() {
+    const { t } = this.props;
+    return (
+      <HashRouter>
+        <div>
+          {<h1>{t('Welcome to React')}</h1>}
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/profile">Profile</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><button href onClick={() => this.changeLanguage("it-IT")}>It</button></li>
+            <li><button onClick={() => this.changeLanguage("en-EN")}>EN</button></li>
+          </ul>
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </HashRouter>
-  );
+      </HashRouter>
+    )
+  }
 }
 
-export default App;
+const MainAppTranslated = withTranslation()(MainApp)
+
+export default function App() {
+  return (
+    <Suspense fallback="loading">
+      <MainAppTranslated />
+    </Suspense>
+  );
+}
